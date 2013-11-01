b42cf2c9e46c89721a79cec57818d3a4fd857514
Andrew Gilbert <andrewg800@gmail.com>
2013-10-31 23:24:03 -0700

    Write test battery with new TestCase tools

--- src/edu/calpoly/stat312/StructuresDuel/Main.java
+++ src/edu/calpoly/stat312/StructuresDuel/Main.java
@@ -1,18 +1,47 @@
 package edu.calpoly.stat312.StructuresDuel;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
-		RandomData randGen = new RandomData();
-		Map<String, String> data = randGen.getDataBlock(DataType.RANDOM,
-				DataType.RANDOM, 1000000);
-		Timer test = Tester.runTest(Type.BINARY_SEARCH_TREE, data,
-				Operation.INSERT);
-		System.out.println(test.elapsedNanoTime() * 1e-9);
-	}
+		List<TestCase> type = new ArrayList<>();
+		List<TestCase> operation = new ArrayList<>();
+		List<TestCase> count = new ArrayList<>();
 
+		// Base instance from which all count instances are derived
+		int[] sizes = { 100, 10000, 1000000, 10000000 };
+		for (int i : sizes) {
+			count.add(new TestCase(DataType.RANDOM, DataType.RANDOM, i));
+		}
+
+		Operation[] ops = { Operation.INSERT, Operation.RETRIEVE,
+				Operation.DELETE };
+		for (Operation o : ops) {
+			operation.add(new TestCase(o));
+		}
+
+		Type[] types = { Type.HASHTABLE, Type.BINARY_SEARCH_TREE, Type.TRIE };
+		for (Type t : types) {
+			type.add(new TestCase(t));
+		}
+
+		// Generate a full-factorial regimen of test cases
+		List<TestCase> cases = TestCase.cartesianProd(type, TestCase.Part.TYPE,
+				operation, TestCase.Part.OPERATION);
+		cases = TestCase.cartesianProd(cases, count, TestCase.Part.GENSETTINGS);
+
+		// Generate a number of replicates of test cases
+		cases = TestCase.replicate(cases, 10);
+
+		// Randomize the test order
+		TestCase.shuffle(cases);
+
+		// Run the tests
+		TestCase.runList(cases);
+
+	}
 	/*
 	 * What do we need to be able to do? - Run replicates (probably blocked on
 	 * time) - Control the data fed to our structures - Generate randomish data
@@ -20,5 +49,4 @@ public class Main {
 	 * Control the operation run on our structure - Feed data to our structures
 	 * in blocks & retrieve it in blocks
 	 */
-
 }

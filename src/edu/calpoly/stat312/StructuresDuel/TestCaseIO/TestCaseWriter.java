b32a0a5f855940fdc82e3a16a571ea7de94378e2
Andrew Gilbert <andrewg800@gmail.com>
2013-11-01 00:42:25 -0700

    Interface for things that can output TestCases

---
+++ src/edu/calpoly/stat312/StructuresDuel/TestCaseIO/TestCaseWriter.java
@@ -0,0 +1,7 @@
+package edu.calpoly.stat312.StructuresDuel.TestCaseIO;
+
+import edu.calpoly.stat312.StructuresDuel.TestCase;
+
+public interface TestCaseWriter {
+	public void write(TestCase t);
+}

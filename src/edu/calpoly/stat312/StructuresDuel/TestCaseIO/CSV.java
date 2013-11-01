aea97ca54862f0ca90f3742fe97a9ef357cdd26d
Andrew Gilbert <andrewg800@gmail.com>
2013-11-01 00:35:52 -0700

    Begin writing class to output TestCases to CSV

---
+++ src/edu/calpoly/stat312/StructuresDuel/TestCaseIO/CSV.java
@@ -0,0 +1,17 @@
+package edu.calpoly.stat312.StructuresDuel.TestCaseIO;
+
+import edu.calpoly.stat312.StructuresDuel.TestCase;
+
+public class CSV implements TestCaseWriter {
+	
+	
+	
+	public CSV(String filename) {
+		
+	}
+
+	@Override
+	public void write(TestCase t) {
+	}
+	
+}

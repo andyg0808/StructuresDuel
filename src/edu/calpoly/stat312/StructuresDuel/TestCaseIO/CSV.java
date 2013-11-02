d3c2702c4e971609b7db5a269ff1eec5a0d52c18
Andrew Gilbert <andrewg800@gmail.com>
2013-11-02 02:16:19 -0700

    Write writer for TestCase to CSV

--- src/edu/calpoly/stat312/StructuresDuel/TestCaseIO/CSV.java
+++ src/edu/calpoly/stat312/StructuresDuel/TestCaseIO/CSV.java
@@ -1,17 +1,76 @@
 package edu.calpoly.stat312.StructuresDuel.TestCaseIO;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
 import edu.calpoly.stat312.StructuresDuel.TestCase;
 
 public class CSV implements TestCaseWriter {
-	
-	
-	
-	public CSV(String filename) {
-		
+
+	/**
+	 * The writer for this object
+	 */
+	private ICsvBeanWriter csvWriter;
+	private boolean started;
+
+	private static CellProcessor[] getProcessors() {
+		final CellProcessor[] processors = new CellProcessor[] {
+				new Optional(), // id
+				new Optional(), // type
+				new Optional(), // op
+				new Optional(), // dataCount
+				new Optional() // nanoTime
+		};
+
+		return processors;
+	}
+
+	public CSV(String filename) throws IOException {
+		csvWriter = new CsvBeanWriter(new FileWriter(filename),
+				CsvPreference.STANDARD_PREFERENCE);
+	}
+
+	/**
+	 * Writes the header for the CSV file
+	 * @throws IOException
+	 */
+	private void start() throws IOException {
+		csvWriter.writeHeader(TestBean.getHeader());
+		started = true;
 	}
 
 	@Override
-	public void write(TestCase t) {
+	public void write(TestCase t) throws IOException {
+		if (!started) {
+			start();
+		}
+
+		TestBean b = new TestBean(t);
+
+		csvWriter.write(b, TestBean.getHeader(), getProcessors());
 	}
+
+	@Override
+	public void writeAll(List<? extends TestCase> l) throws IOException {
+		if (!started) {
+			start();
+		}
+		for (TestCase c : l) {
+			TestBean b = new TestBean(c);
+			csvWriter.write(b, TestBean.getHeader(), getProcessors());
+		}
+	}
+
+	public void close() throws IOException {
+		csvWriter.close();
+	}
+	
 	
 }

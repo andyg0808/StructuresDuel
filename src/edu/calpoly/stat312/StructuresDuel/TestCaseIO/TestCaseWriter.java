54aeca151530ad9301876e14eea66136e4d5c906
Andrew Gilbert <andrewg800@gmail.com>
2013-11-02 02:16:05 -0700

    Complete interface for writers that know what to do with a TestCase

--- src/edu/calpoly/stat312/StructuresDuel/TestCaseIO/TestCaseWriter.java
+++ src/edu/calpoly/stat312/StructuresDuel/TestCaseIO/TestCaseWriter.java
@@ -1,7 +1,27 @@
 package edu.calpoly.stat312.StructuresDuel.TestCaseIO;
 
+import java.io.IOException;
+import java.util.List;
+
 import edu.calpoly.stat312.StructuresDuel.TestCase;
 
 public interface TestCaseWriter {
-	public void write(TestCase t);
+	/**
+	 * Write the TestCase to the destination for this TestCaseWriter
+	 * 
+	 * @param t
+	 *            The TestCase to write
+	 * @throws IOException
+	 */
+	public void write(TestCase t) throws IOException;
+
+	/**
+	 * Write all the TestCases in the list to the destination for this
+	 * TestCaseWriter.
+	 * 
+	 * @param l
+	 *            The List of TestCases to write
+	 * @throws IOException
+	 */
+	public void writeAll(List<? extends TestCase> l) throws IOException;
 }
